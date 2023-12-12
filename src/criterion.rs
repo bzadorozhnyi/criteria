daa03638cf6cc6c073ac8b662bee4182e46125bb
Bohdan Zadorozhnyi <bohzador@gmail.com>
2023-12-12 17:42:44 +0200

    Implemented criterions of a risk condition.

--- src/criterion.rs
+++ src/criterion.rs
@@ -72,43 +72,187 @@ pub fn savage(a: &Vec<Vec<f32>>) -> (f32, Vec<usize>) {
     return (answer, get_indeces(&z, answer));
 }
 
+fn bayes(a: &Vec<Vec<f32>>, p: &Vec<f32>) -> (f32, Vec<usize>) {
+    let z = a
+        .iter()
+        .map(|row| {
+            row.iter()
+                .zip(p.iter())
+                .map(|(value, probability)| value * probability)
+                .sum()
+        })
+        .collect();
+
+    let answer = get_max(&z);
+
+    return (answer, get_indeces(&z, answer));
+}
+
+fn dispersion_minimization(a: &Vec<Vec<f32>>, p: &Vec<f32>) -> (f32, Vec<usize>) {
+    let z_squared = a
+        .iter()
+        .map(|row| {
+            row.iter()
+                .zip(p.iter())
+                .map(|(value, probability)| value.powi(2) * probability)
+                .sum::<f32>()
+                - row
+                    .iter()
+                    .zip(p.iter())
+                    .map(|(value, probability)| value * probability)
+                    .sum::<f32>()
+                    .powi(2)
+        })
+        .collect();
+
+    let answer_squared = get_min(&z_squared);
+
+    return (
+        answer_squared.sqrt(),
+        get_indeces(&z_squared, answer_squared),
+    );
+}
+
+fn probability_maximization(a: &Vec<Vec<f32>>, p: &Vec<f32>, b: f32) -> (f32, Vec<usize>) {
+    let z = a
+        .iter()
+        .map(|row| {
+            row.iter()
+                .zip(p.iter())
+                .map(|(value, probability)| if value > &b { probability } else { &0.0 })
+                .sum()
+        })
+        .collect();
+
+    let answer = get_max(&z);
+
+    return (answer, get_indeces(&z, answer));
+}
+
+fn modal(a: &Vec<Vec<f32>>, p: &Vec<f32>) -> Option<(f32, Vec<usize>)> {
+    let max_probability = get_max(&p);
+
+    if p.iter().filter(|&value| *value == max_probability).count() > 1 {
+        return None;
+    }
+
+    let max_probability_element_index = p
+        .iter()
+        .position(|&value| value == max_probability)
+        .unwrap();
+
+    let z = a
+        .iter()
+        .map(|row| row[max_probability_element_index])
+        .collect();
+
+    let answer = get_max(&z);
+
+    return Some((answer, get_indeces(&z, answer)));
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::criterion::{hurwitz, maximax, minimax, savage};
+    mod uncertainty {
+        use crate::criterion::{hurwitz, maximax, minimax, savage};
 
-    fn generate_data() -> Vec<Vec<f32>> {
-        vec![vec![45.0, 25.0, 50.0], vec![20.0, 60.0, 25.0]]
+        fn generate_test_data() -> Vec<Vec<f32>> {
+            vec![vec![45.0, 25.0, 50.0], vec![20.0, 60.0, 25.0]]
+        }
+
+        #[test]
+        fn test_maximax() {
+            let a = generate_test_data();
+
+            assert_eq!(maximax(&a).0, 60.0, "Maximax gives incorrect result.");
+        }
+
+        #[test]
+        fn test_minimax() {
+            let a = generate_test_data();
+
+            assert_eq!(minimax(&a).0, 25.0, "Maximax gives incorrect result.");
+        }
+
+        #[test]
+        fn test_hurwitz() {
+            let a = generate_test_data();
+
+            assert_eq!(
+                hurwitz(&a, 0.3).0,
+                32.5,
+                "Hurwitz gives incorrect result for alpha = 0.3"
+            );
+        }
+
+        #[test]
+        fn test_savage() {
+            let a = generate_test_data();
+
+            assert_eq!(savage(&a).0, 25.0, "Savage gives incorrect result.");
+        }
     }
 
-    #[test]
-    fn test_maximax() {
-        let a = generate_data();
+    mod risk_condition {
+        use crate::criterion::{bayes, dispersion_minimization, modal, probability_maximization};
 
-        assert_eq!(maximax(&a).0, 60.0, "Maximax gives incorrect result.");
-    }
+        fn generate_test_data() -> (Vec<Vec<f32>>, Vec<f32>) {
+            let a = vec![
+                vec![100_000.0, -50_000.0, -50_000.0],
+                vec![-50_000.0, -50_000.0, 100_000.0],
+                vec![15_000.0, 15_000.0, 0.0],
+                vec![0.0, 0.0, 0.0],
+            ];
 
-    #[test]
-    fn test_minimax() {
-        let a = generate_data();
+            let p = vec![0.5, 0.1, 0.4];
 
-        assert_eq!(minimax(&a).0, 25.0, "Maximax gives incorrect result.");
-    }
+            (a, p)
+        }
 
-    #[test]
-    fn test_hurwitz() {
-        let a = generate_data();
+        #[test]
+        fn test_bayes() {
+            let (a, p) = generate_test_data();
 
-        assert_eq!(
-            hurwitz(&a, 0.3).0,
-            32.5,
-            "Hurwitz gives incorrect result for alpha = 0.3"
-        );
-    }
+            assert_eq!(bayes(&a, &p).0, 25_000.0, "Bayes gives incorrect result.")
+        }
 
-    #[test]
-    fn test_savage() {
-        let a = generate_data();
+        #[test]
+        fn test_dispersion_minimization() {
+            let (a, p) = generate_test_data();
 
-        assert_eq!(savage(&a).0, 25.0, "Savage giver incorrect result.");
+            assert_eq!(
+                dispersion_minimization(&a, &p).0,
+                0.0,
+                "Dispersion minimization gives incorrect result."
+            );
+        }
+
+        #[test]
+        fn test_probability_maximization() {
+            let (a, p) = generate_test_data();
+
+            assert_eq!(
+                probability_maximization(&a, &p, 40_000.0).0,
+                0.5,
+                "Probability maximization gives incorrect result for a = 40_000."
+            );
+
+            assert_eq!(
+                probability_maximization(&a, &p, 10_000.0).0,
+                0.6,
+                "Probability maximization gives incorrect result for a = 10_000."
+            )
+        }
+
+        #[test]
+        fn test_modal() {
+            let (a, p) = generate_test_data();
+
+            assert_eq!(
+                modal(&a, &p).unwrap().0,
+                100_000.0,
+                "Modal gives incorrect result."
+            )
+        }
     }
 }
